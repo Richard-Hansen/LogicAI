945214048c887522d39a9b6d41075993748a6df8
colholt <columbus@purdue.edu>
2019-09-17 23:14:22 -0400

    Store/retrieve board/difficulty

--- Backend/Fractal-AI-Game/game/components/boardSelector.js
+++ Backend/Fractal-AI-Game/game/components/boardSelector.js
@@ -10,7 +10,7 @@ class boardSelector {
         this.boardContainer.style('justify-content', 'space-between')
         this.boardContainer.style('overflow', 'scroll')
         this.boardContainer.style('top', '30%')
-        this.boardContainer.style('left', '60%')
+        this.boardContainer.style('left', '65%')
         this.boardContainer.style('margin-left', -1 * 400 / 2 + 'px')
         this.buttons = []
         for (var i = 0; i < 20; i += 1) {
@@ -22,10 +22,17 @@ class boardSelector {
             c.mousePressed(this.changeBoard(i))
             this.buttons.push(c)
         }
+        // Retrieve board from local storage
+        board = window.localStorage.getItem("board")
+        // Calling curried functions requires calling the second function hence ()()
+        this.changeBoard(board)()
     }
 
     changeBoard = e => () => {
+        if (e === undefined)
+            return
         board = e
+        window.localStorage.setItem("board", e)
         this.buttons.forEach(element => {
             element.style('background-color', 'rgba(255,0,0,0.1)')
         });
--- Backend/Fractal-AI-Game/game/components/difficultySelector.js
+++ Backend/Fractal-AI-Game/game/components/difficultySelector.js
@@ -8,45 +8,67 @@ class difficultySelector {
         this.difficultyContainer.style('flex-direction', 'column')
         this.difficultyContainer.style('justify-content', 'space-between')
         this.difficultyContainer.style('top', '30%')
-        this.difficultyContainer.style('left', '30%')
+        this.difficultyContainer.style('left', '35%')
         this.difficultyContainer.style('margin-left', -1 * 400 / 2 + 'px')
         this.easyButton = createButton('Easy')
         this.easyButton.parent(this.difficultyContainer)
         this.easyButton.style('width', '100%')
-        this.easyButton.style('height', '120px')
+        this.easyButton.style('height', '90px')
         this.mediumButton = createButton('Medium')
         this.mediumButton.parent(this.difficultyContainer)
         this.mediumButton.style('width', '100%')
-        this.mediumButton.style('height', '120px')
+        this.mediumButton.style('height', '90px')
         this.hardButton = createButton('Hard')
         this.hardButton.parent(this.difficultyContainer)
         this.hardButton.style('width', '100%')
-        this.hardButton.style('height', '120px')
+        this.hardButton.style('height', '90px')
+        this.extremeButton = createButton('Impossible')
+        this.extremeButton.parent(this.difficultyContainer)
+        this.extremeButton.style('width', '100%')
+        this.extremeButton.style('height', '90px')
         this.easyButton.style('background-color', 'rgba(255,0,0,0.1)')
         this.mediumButton.style('background-color', 'rgba(255,0,0,0.1)')
         this.hardButton.style('background-color', 'rgba(255,0,0,0.1)')
+        this.extremeButton.style('background-color', 'rgba(255,0,0,0.1)')
         this.easyButton.mousePressed(this.changeDifficulty('easy'))
         this.mediumButton.mousePressed(this.changeDifficulty('medium'))
         this.hardButton.mousePressed(this.changeDifficulty('hard'))
+        this.extremeButton.mousePressed(this.changeDifficulty('impossible'))
+        // Retrieve difficulty from local storage
+        difficulty = window.localStorage.getItem("difficulty")
+        // Calling curried functions requires calling the second function hence ()()
+        this.changeDifficulty(difficulty)()
     }
 
     changeDifficulty = e => () => {
+        if (e === undefined)
+            return
         difficulty = e
+        window.localStorage.setItem("difficulty", e)
         switch (e) {
             case 'easy':
                 this.easyButton.style('background-color', 'rgba(255,0,0,0.6)')
                 this.mediumButton.style('background-color', 'rgba(255,0,0,0.1)')
                 this.hardButton.style('background-color', 'rgba(255,0,0,0.1)')
+                this.extremeButton.style('background-color', 'rgba(255,0,0,0.1)')
                 break;
             case 'medium': ;
                 this.easyButton.style('background-color', 'rgba(255,0,0,0.1)')
                 this.mediumButton.style('background-color', 'rgba(255,0,0,0.6)')
                 this.hardButton.style('background-color', 'rgba(255,0,0,0.1)')
+                this.extremeButton.style('background-color', 'rgba(255,0,0,0.1)')
                 break;
             case 'hard':
                 this.easyButton.style('background-color', 'rgba(255,0,0,0.1)')
                 this.mediumButton.style('background-color', 'rgba(255,0,0,0.1)')
                 this.hardButton.style('background-color', 'rgba(255,0,0,0.6)')
+                this.extremeButton.style('background-color', 'rgba(255,0,0,0.1)')
+                break;
+            case 'impossible':
+                this.easyButton.style('background-color', 'rgba(255,0,0,0.1)')
+                this.mediumButton.style('background-color', 'rgba(255,0,0,0.1)')
+                this.hardButton.style('background-color', 'rgba(255,0,0,0.1)')
+                this.extremeButton.style('background-color', 'rgba(255,0,0,0.6)')
                 break;
             default:
                 break;
