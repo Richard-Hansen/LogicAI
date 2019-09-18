42d433f9b128f4fcbf95cb8cc8d22873101b3ed7
colholt <columbus@purdue.edu>
2019-09-18 01:03:11 -0400

    added tests and compatibility

--- Backend/Fractal-AI-Game/game/components/boardSelector.js
+++ Backend/Fractal-AI-Game/game/components/boardSelector.js
@@ -1,49 +1,62 @@
-class boardSelector {
+class BoardSelector {
     constructor() {
-        this.boardContainer = createDiv()
-        this.boardContainer.style('position', 'absolute')
-        this.boardContainer.style('width', '400px')
-        this.boardContainer.style('height', '360px')
-        this.boardContainer.style('border', '4px solid black')
-        this.boardContainer.style('display', 'flex')
-        this.boardContainer.style('flex-direction', 'column')
-        this.boardContainer.style('justify-content', 'space-between')
-        this.boardContainer.style('overflow', 'scroll')
-        this.boardContainer.style('top', '30%')
-        this.boardContainer.style('left', '65%')
-        this.boardContainer.style('margin-left', -1 * 400 / 2 + 'px')
+        try {
+            this.boardContainer = createDiv()
+            this.boardContainer.style('position', 'absolute')
+            this.boardContainer.style('width', '400px')
+            this.boardContainer.style('height', '360px')
+            this.boardContainer.style('border', '4px solid black')
+            this.boardContainer.style('display', 'flex')
+            this.boardContainer.style('flex-direction', 'column')
+            this.boardContainer.style('justify-content', 'space-between')
+            this.boardContainer.style('overflow', 'scroll')
+            this.boardContainer.style('top', '30%')
+            this.boardContainer.style('left', '65%')
+            this.boardContainer.style('margin-left', -1 * 400 / 2 + 'px')
+        } catch (err) {
+
+        }
         this.buttons = []
         for (var i = 0; i < 20; i += 1) {
-            var c = createButton((i + 1).toString())
-            c.parent(this.boardContainer)
-            c.style('width', '100%')
-            c.style('height', '40px')
-            c.style('background-color', 'rgba(255,0,0,0.1)')
-            c.mousePressed(this.changeBoard(i))
-            this.buttons.push(c)
+            let fnc = function () {
+                this.changeBoard(i)
+            }
+            try {
+                var c = createButton((i + 1).toString())
+                c.parent(this.boardContainer)
+                c.style('width', '100%')
+                c.style('height', '40px')
+                c.style('background-color', 'rgba(255,0,0,0.1)')
+                c.mousePressed(fnc)
+                this.buttons.push(c)
+            } catch (err) { }
         }
         // Retrieve board from local storage
         board = window.localStorage.getItem("board")
         // Calling curried functions requires calling the second function hence ()()
-        this.changeBoard(board)()
+        this.changeBoard(board)
     }
 
-    changeBoard = e => () => {
+    changeBoard(e) {
         if (e === undefined)
             return
         board = e
         window.localStorage.setItem("board", e)
-        this.buttons.forEach(element => {
-            element.style('background-color', 'rgba(255,0,0,0.1)')
-        });
-        this.buttons[e].style('background-color', 'rgba(255,0,0,0.6)')
+        try {
+            this.buttons.forEach(element => {
+                element.style('background-color', 'rgba(255,0,0,0.1)')
+            });
+            this.buttons[e].style('background-color', 'rgba(255,0,0,0.6)')
+        } catch (err) { }
     }
 
-    hide = () => {
+    hide() {
         this.boardContainer.hide()
     }
 
-    show = () => {
+    show() {
         this.boardContainer.show()
     }
 }
+
+module.exports = BoardSelector;
