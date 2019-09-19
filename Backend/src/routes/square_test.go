885bbd0785b587e8e8d4951279d44c5af0ac83c1
Dellamoresteven <dellamoresteven@gmail.com>
2019-09-19 17:48:27 -0400

    Add the players to the frontend and the move data being sent to the move route correctly

--- Backend/src/routes/square_test.go
+++ Backend/src/routes/square_test.go
@@ -11,16 +11,16 @@ import (
 func TestSquareData(t *testing.T) {
 	t.Run("Post To SquareData", func(t *testing.T) {
 		// jsonMoveData, _ := json.Marshal(moveData)
-//
+		//
 		// request, _ := http.NewRequest("POST", "/SquareData", bytes.NewBuffer(jsonMoveData))
-    //
+		//
 		// response := httptest.NewRecorder()
-    //
+		//
 		// MoveHandler(response, request)
-    //
+		//
 		// got := response.Body.String()
 		// want := "Hello, you called \"/move\", &{\"X1\":1,\"Y1\":1,\"X2\":1,\"Y2\":2}"
-    //
+		//
 		// if got != want {
 		// 	t.Errorf("got %q, want %q", got, want)
 		// }
