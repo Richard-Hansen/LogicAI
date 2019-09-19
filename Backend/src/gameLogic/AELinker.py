81a8523c8f508ea4ba362b7041d6f3e32b21fb4c
javaComSci <iramanat@purdue.edu>
2019-09-19 18:44:07 -0400

    Work

--- Backend/src/gameLogic/AELinker.py
+++ Backend/src/gameLogic/AELinker.py
@@ -1,5 +1,5 @@
-# from agents import AgentX86, Agentling
-# from environment import Environment, Envy
+from agents import AgentX86, Agentling
+from environment import Environment, Envy
 
 # does the mapping from an edge to the vertices for all the edges
 def map_edge_and_vertex():
@@ -56,27 +56,24 @@ edge_to_verticies = {
 # maps each vertex pair to an edge
 vertex_pair_to_edge = {(0, 5): 0, (5, 10): 1, (10, 15): 2, (15, 20): 3, (1, 6): 4, (6, 11): 5, (11, 16): 6, (16, 21): 7, (2, 7): 8, (7, 12): 9, (12, 17): 10, (17, 22): 11, (3, 8): 12, (8, 13): 13, (13, 18): 14, (18, 23): 15, (4, 9): 16, (9, 14): 17, (14, 19): 18, (19, 24): 19, (0, 1): 20, (5, 6): 21, (10, 11): 22, (15, 16): 23, (20, 21): 24, (1, 2): 25, (6, 7): 26, (11, 12): 27, (16, 17): 28, (21, 22): 29, (2, 3): 30, (7, 8): 31, (12, 13): 32, (17, 18): 33, (22, 23): 34, (3, 4): 35, (8, 9): 36, (13, 14): 37, (18, 19): 38, (23, 24): 39}
 
-# # set up the big agent and environment
-# a = [0.045625000000000006, 0.07812499999999999, 0.08125000000000002, 0.11093750000000004, 0.03875, 0.08125000000000007, 0.071875, 0.03593750000000004, 0.06124999999999997, 0.08593749999999997, 0.02656249999999999, 0.046875000000000014, 0.038750000000000014, 0.090625, 0.06562499999999999, 0.04062500000000001]
-# areas = []
-# for i in range(0,4):
-# 	areas.append(a[i],a[i+4],a[i+8],a[i+12])
+# set up the big agent and environment
+a = [0.045625000000000006, 0.07812499999999999, 0.08125000000000002, 0.11093750000000004, 0.03875, 0.08125000000000007, 0.071875, 0.03593750000000004, 0.06124999999999997, 0.08593749999999997, 0.02656249999999999, 0.046875000000000014, 0.038750000000000014, 0.090625, 0.06562499999999999, 0.04062500000000001]
+areas = []
+for i in range(0,4):
+	areas.append(a[i])
+	areas.append(a[i+4])
+	areas.append(a[i+8])
+	areas.append(a[i+12])
 
-# # Environment
-# environment = Environment(areas)
-# environment.create_envy_states()
+# Environment - creates the environment and creates all the states for the environment
+environment = Environment(areas)
+environment.create_envy_states()
 
-# # Agent X86
+# Agent X86 - creates the agent for the environment
 # agentX86 = AgentX86(environment)
 
 
-# def take_action(verticies):
-# 	# get the list of verticies that would need to correspond to the edges
-# 	edges_taken = []
-# 	for vertex_pair in verticies:
-# 		edges_taken.append(vertex_pair_to_edge[vertex_pair])
-# 	# return the result of the taken action
-# 	return agentX86.X_take_action(edges_taken)
+
 
 
 # have an agent with its agentlings connected to the environment
