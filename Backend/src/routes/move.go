6f6153d146e0de36723382fc37375f8a547db8b5
Dellamoresteven <dellamoresteven@gmail.com>
2019-09-20 01:12:16 -0400

    l

--- Backend/src/routes/move.go
+++ Backend/src/routes/move.go
@@ -1,22 +1,23 @@
 /* define this file as part of the pacakge `routes` */
 package routes
+
 //package main
 
 import (
 	"C"
+	"database/sql"
 	"encoding/json"
 	"fmt"
-	"html"
-	"database/sql"
+	// "html"
 	"io/ioutil"
-	http "net/http"
 	"math"
+	http "net/http"
 )
 
 /* Everything is going to be put into these two 2d int arrays. */
 type MoveDataType struct {
-	EdgesSquare     [][]int      `json: "edgesSquare"`
-	OwnerSquare     [][]int      `json: "ownerSquare"`
+	EdgesSquare [][]int `json: "edgesSquare"`
+	OwnerSquare [][]int `json: "ownerSquare"`
 }
 
 var p1 int
@@ -31,96 +32,98 @@ func MoveHandler(w http.ResponseWriter, r *http.Request) {
 	defer fmt.Println("End MoveHandler")
 
 	/* Making an empty struct object */
-	mapData := MoveDataType{};
+	mapData := MoveDataType{}
 
-	//Reading the body of the request, r 
+	//Reading the body of the request, r
 	reqBody, _ := ioutil.ReadAll(r.Body)
 
 	/* Marshal data into a the struct from above */
 	_ = json.Unmarshal(reqBody, &mapData)
-	
-	fmt.Println(TakeAction(mapData))
+
+	// fmt.Println(TakeAction(mapData))
 
 	/* Printing what we got! */
 	fmt.Println(mapData)
 	/* Forming a response */
-	fmt.Fprintf(w, "Hello, you called %q, %s", html.EscapeString(r.URL.Path), &reqBody)
+	fmt.Fprintf(w, "%s", TakeAction(mapData))
 
 }
 
-func TakeAction(mapData MoveDataType) [2]int {
+func TakeAction(mapData MoveDataType) string {
 	p1 = 1
+	fmt.Println("Hello1")
 	/*maps a pair a vertices to an edge on the 4x4 board.*/
-	vertices_to_edges := map[[2]int]int {
-		[2]int{0, 5}: 0,
-		[2]int{5, 10}: 1,
+	vertices_to_edges := map[[2]int]int{
+		[2]int{0, 5}:   0,
+		[2]int{5, 10}:  1,
 		[2]int{10, 15}: 2,
 		[2]int{15, 20}: 3,
-		[2]int{1, 6}: 4,
-		[2]int{6, 11}: 5,
+		[2]int{1, 6}:   4,
+		[2]int{6, 11}:  5,
 		[2]int{11, 16}: 6,
 		[2]int{16, 21}: 7,
-		[2]int{2, 7}: 8,
-		[2]int{7, 12}: 9,
+		[2]int{2, 7}:   8,
+		[2]int{7, 12}:  9,
 		[2]int{12, 17}: 10,
 		[2]int{17, 22}: 11,
-		[2]int{3, 8}: 12,
-		[2]int{8, 13}: 13,
+		[2]int{3, 8}:   12,
+		[2]int{8, 13}:  13,
 		[2]int{13, 18}: 14,
 		[2]int{18, 23}: 15,
-		[2]int{4, 9}: 16,
-		[2]int{9, 14}: 17,
+		[2]int{4, 9}:   16,
+		[2]int{9, 14}:  17,
 		[2]int{14, 19}: 18,
 		[2]int{19, 24}: 19,
-		[2]int{0, 1}: 20,
-		[2]int{5, 6}: 21,
+		[2]int{0, 1}:   20,
+		[2]int{5, 6}:   21,
 		[2]int{10, 11}: 22,
 		[2]int{15, 16}: 23,
 		[2]int{20, 21}: 24,
-		[2]int{1, 2}: 25,
-		[2]int{6, 7}: 26,
+		[2]int{1, 2}:   25,
+		[2]int{6, 7}:   26,
 		[2]int{11, 12}: 27,
 		[2]int{16, 17}: 28,
 		[2]int{21, 22}: 29,
-		[2]int{2, 3}: 30,
-		[2]int{7, 8}: 31,
+		[2]int{2, 3}:   30,
+		[2]int{7, 8}:   31,
 		[2]int{12, 13}: 32,
 		[2]int{17, 18}: 33,
 		[2]int{22, 23}: 34,
-		[2]int{3, 4}: 35,
-		[2]int{8, 9}: 36,
+		[2]int{3, 4}:   35,
+		[2]int{8, 9}:   36,
 		[2]int{13, 14}: 37,
 		[2]int{18, 19}: 38,
 		[2]int{23, 24}: 39,
 	}
+	fmt.Println("Hello2")
 
 	/* maps all vertices into the big_board_edges array, the index in the array corresponds to which player owns the edge */
 	var big_board_edges [40]int
 	edges := mapData.EdgesSquare
-	for i:=0; i < len(edges); i++ {
-		ind := vertices_to_edges[[2]int{edges[i][0],edges[i][1]}]
+	for i := 0; i < len(edges); i++ {
+		ind := vertices_to_edges[[2]int{edges[i][0], edges[i][1]}]
 		big_board_edges[ind] = edges[i][2]
 	}
 
 	/* maps all squares taken into the array big_board_squares, the index in the array corresponds to which palyer owns the square*/
 	var big_board_squares [16]int
 	squares := mapData.OwnerSquare
-	for i:=0; i < len(squares); i++ {
+	for i := 0; i < len(squares); i++ {
 		big_board_squares[squares[i][0]] = squares[i][1]
 	}
 
 	/* Maps edges from the big_board_edges to the [[a,e],..], where 'a' is the agentling and 'e' is the edge on the 2x2 for the agentling*/
-	big_to_tiny_edges := map[int][][]int {
-		0: [][]int{{0, 0}},
-		1: [][]int{{0, 1}, {1, 0}},
-		2: [][]int{{1, 1}, {2, 0}},
-		3: [][]int{{2, 1}},
-		4: [][]int{{0, 2}, {3, 0}},
-		5: [][]int{{0, 3}, {1, 2}, {3, 1}, {4, 0}},
-		6: [][]int{{1, 3}, {2, 2}, {4, 1}, {5, 0}},
-		7: [][]int{{2, 3}, {5, 1}},
-		8: [][]int{{0, 4}, {3, 2}, {6, 0}},
-		9: [][]int{{0, 5}, {1, 4}, {3, 3}, {4, 2}, {6, 1}, {7, 0}},
+	big_to_tiny_edges := map[int][][]int{
+		0:  [][]int{{0, 0}},
+		1:  [][]int{{0, 1}, {1, 0}},
+		2:  [][]int{{1, 1}, {2, 0}},
+		3:  [][]int{{2, 1}},
+		4:  [][]int{{0, 2}, {3, 0}},
+		5:  [][]int{{0, 3}, {1, 2}, {3, 1}, {4, 0}},
+		6:  [][]int{{1, 3}, {2, 2}, {4, 1}, {5, 0}},
+		7:  [][]int{{2, 3}, {5, 1}},
+		8:  [][]int{{0, 4}, {3, 2}, {6, 0}},
+		9:  [][]int{{0, 5}, {1, 4}, {3, 3}, {4, 2}, {6, 1}, {7, 0}},
 		10: [][]int{{1, 5}, {2, 4}, {4, 3}, {5, 2}, {7, 1}, {8, 0}},
 		11: [][]int{{2, 5}, {5, 3}, {8, 1}},
 		12: [][]int{{3, 4}, {6, 2}},
@@ -152,11 +155,12 @@ func TakeAction(mapData MoveDataType) [2]int {
 		38: [][]int{{7, 11}, {8, 10}},
 		39: [][]int{{8, 11}},
 	}
+	fmt.Println("Hello3")
 
 	/* Calculate all 9 2x2 environments for the agentlings*/
 	var tiny_envys [9][16]int
-	for i:=0; i < 40; i++ {
-		for j:=0; j<len(big_to_tiny_edges[i]); j++ {
+	for i := 0; i < 40; i++ {
+		for j := 0; j < len(big_to_tiny_edges[i]); j++ {
 			agentling := big_to_tiny_edges[i][j][0]
 			agentling_edge := big_to_tiny_edges[i][j][1]
 			tiny_envys[agentling][agentling_edge] = big_board_edges[i]
@@ -164,17 +168,17 @@ func TakeAction(mapData MoveDataType) [2]int {
 	}
 
 	/* Maps squares from the big_board_edges to the [[a,s],..], where 'a' is the agentling and 's' is the square on the 2x2 for the agentling*/
-	big_to_tiny_squares := map[int][][]int {
-		0: [][]int{{0, 0}},
-		1: [][]int{{0, 2}, {3, 0}},
-		2: [][]int{{3, 2}, {6, 0}},
-		3: [][]int{{6, 2}},
-		4: [][]int{{0, 1}, {1, 0}},
-		5: [][]int{{0, 3}, {1, 2}, {3, 1}, {4, 0}},
-		6: [][]int{{3, 3}, {4, 2}, {6, 1}, {7, 0}},
-		7: [][]int{{6, 3}, {7, 2}},
-		8: [][]int{{1, 1}, {2, 0}},
-		9: [][]int{{1, 3}, {2, 2}, {4, 1}, {5, 0}},
+	big_to_tiny_squares := map[int][][]int{
+		0:  [][]int{{0, 0}},
+		1:  [][]int{{0, 2}, {3, 0}},
+		2:  [][]int{{3, 2}, {6, 0}},
+		3:  [][]int{{6, 2}},
+		4:  [][]int{{0, 1}, {1, 0}},
+		5:  [][]int{{0, 3}, {1, 2}, {3, 1}, {4, 0}},
+		6:  [][]int{{3, 3}, {4, 2}, {6, 1}, {7, 0}},
+		7:  [][]int{{6, 3}, {7, 2}},
+		8:  [][]int{{1, 1}, {2, 0}},
+		9:  [][]int{{1, 3}, {2, 2}, {4, 1}, {5, 0}},
 		10: [][]int{{4, 3}, {5, 2}, {7, 1}, {8, 0}},
 		11: [][]int{{7, 3}, {8, 2}},
 		12: [][]int{{2, 1}},
@@ -184,38 +188,44 @@ func TakeAction(mapData MoveDataType) [2]int {
 	}
 
 	/* Fills the tiny_envys array with the owner of each of the squares that are on the big board */
-	for i:=0; i < 16; i++ {
-		for j:=0; j<len(big_to_tiny_squares[i]); j++ {
+	for i := 0; i < 16; i++ {
+		for j := 0; j < len(big_to_tiny_squares[i]); j++ {
 			agentling := big_to_tiny_squares[i][j][0]
 			agentling_square := big_to_tiny_squares[i][j][1]
-			tiny_envys[agentling][12 + agentling_square] = big_board_squares[i]
+			tiny_envys[agentling][12+agentling_square] = big_board_squares[i]
 		}
 	}
-
-	return calc_action(tiny_envys)
+	fmt.Println("Hello4")
+	ret := calc_action(tiny_envys);
+	fmt.Println("Hello5")
+	rett := fmt.Sprintf("%d %d", ret[0], ret[1])
+	_ = rett;
+	return "0 1";
 }
-
 
 // hash the value for the state for the database
 func HashCode(stateInfo [16]int) string {
 	h := 0
-	var k float64 //Needs to be float64.  
+	fmt.Println("Wut1")
+	var k float64 //Needs to be float64.
 	// reverse and change the info for the state
-	for i:= len(stateInfo); i >= 0; i-- {
-		/** 
-		 * math.Pow returns a float64. Float 64's cannot be * to int's. 
+	for i := len(stateInfo) - 1; i >= 0; i-- {
+		/**
+		 * math.Pow returns a float64. Float 64's cannot be * to int's.
 		 * So we can either cast the math.Pow to an int, or cast stateInfo to a float64
 		 */
 		h = h + (int(math.Pow(3, k)) * stateInfo[i])
 		k += 1
 	}
-	return fmt.Sprintf("%d", h)
+	fmt.Println("Wut2")
+	return fmt.Sprintf("%d", int(h))
 }
 
 // get the value of the hash from the database
 func GetValue(stateInfo [16]int) int {
+	fmt.Println("Goodbye1")
 	hashCode := HashCode(stateInfo)
-
+	fmt.Println("Goodbye2")
 	// make the call to the database
 	db, err := sql.Open("mysql", "Richard:SteveIsTheBest@tcp(198.199.121.101:3306)/logic")
 	defer db.Close()
@@ -223,8 +233,8 @@ func GetValue(stateInfo [16]int) int {
 	if err != nil {
 		panic(err.Error())
 	}
-
-	selectStatement := `SELECT Value FROM hashes WHERE HashCode = ` + hashCode + ";"
+	fmt.Println("Goodbye3")
+	selectStatement := `SELECT Value FROM hashes WHERE HashCode = ` + hashCode + " LIMIT 1;"
 	rows, errs := db.Query(selectStatement)
 	defer rows.Close()
 
@@ -242,42 +252,41 @@ func GetValue(stateInfo [16]int) int {
 func get_action_list(curr_state [16]int) [12]int {
 	var action_list [12]int
 
-	for i:=0;i<12;i++ {
+	for i := 0; i < 12; i++ {
 		action_list[i] = -1
 	}
 
-	for i:=0;i<12;i++ {
+	for i := 0; i < 12; i++ {
 		if curr_state[i] == 0 {
 			action_list[i] = get_edge_value_runner(curr_state, i)
 		}
 	}
 
-	return action_list	
+	return action_list
 }
-
 
 func get_edge_value_runner(curr_state [16]int, edge int) int {
 	//set array as if I had chosen the edge
 	curr_state[edge] = p1
 
-	top_left_square := [4]int{0,2,6,7}
-	top_right_square := [4]int{1,3,7,8}
-	bottom_left_square := [4]int{2,4,9,10}
-	bottom_right_square := [4]int{3,5,10,11}
+	top_left_square := [4]int{0, 2, 6, 7}
+	top_right_square := [4]int{1, 3, 7, 8}
+	bottom_left_square := [4]int{2, 4, 9, 10}
+	bottom_right_square := [4]int{3, 5, 10, 11}
 
-	if curr_state[0] != 0 && curr_state[2] != 0 && curr_state[6] != 0 && curr_state[7] != 0 && check_if_in_list(top_left_square,edge){
+	if curr_state[0] != 0 && curr_state[2] != 0 && curr_state[6] != 0 && curr_state[7] != 0 && check_if_in_list(top_left_square, edge) {
 		curr_state[12] = p1
 	}
 
-	if curr_state[1] != 0 && curr_state[3] != 0 && curr_state[7] != 0 && curr_state[8] != 0 && check_if_in_list(top_right_square,edge){
+	if curr_state[1] != 0 && curr_state[3] != 0 && curr_state[7] != 0 && curr_state[8] != 0 && check_if_in_list(top_right_square, edge) {
 		curr_state[13] = p1
-	} 
+	}
 
-	if curr_state[2] != 0 && curr_state[4] != 0 && curr_state[9] != 0 && curr_state[10] != 0 && check_if_in_list(bottom_left_square,edge){
+	if curr_state[2] != 0 && curr_state[4] != 0 && curr_state[9] != 0 && curr_state[10] != 0 && check_if_in_list(bottom_left_square, edge) {
 		curr_state[14] = p1
-	} 
+	}
 
-	if curr_state[3] != 0 && curr_state[5] != 0 && curr_state[10] != 0 && curr_state[11] != 0 && check_if_in_list(bottom_right_square,edge){
+	if curr_state[3] != 0 && curr_state[5] != 0 && curr_state[10] != 0 && curr_state[11] != 0 && check_if_in_list(bottom_right_square, edge) {
 		curr_state[15] = p1
 	}
 
@@ -285,7 +294,7 @@ func get_edge_value_runner(curr_state [16]int, edge int) int {
 }
 
 func check_if_in_list(list [4]int, v int) bool {
-	for i:=0;i<4;i++ {
+	for i := 0; i < 4; i++ {
 		if list[i] == v {
 			return true
 		}
@@ -294,110 +303,105 @@ func check_if_in_list(list [4]int, v int) bool {
 }
 
 func calc_action(tiny_envys [9][16]int) [2]int {
-		var big_board_sums [40]int
-		var big_board_counts [40]int
+	var big_board_sums [40]int
+	var big_board_counts [40]int
 
-		tiny_to_big := map[int][]int {
-						0:{0,1,4,5,8,9,20,21,22,25,26,27},
-						1:{1,2,5,6,9,10,21,22,23,26,27,28},
-						2:{2,3,6,7,10,11,22,23,24,27,28,29},
-						3:{4,5,8,9,12,13,25,26,27,30,31,32},
-						4:{5,6,9,10,13,14,26,27,28,31,32,33},
-						5:{6,7,10,11,14,15,27,28,29,32,33,34},
-						6:{8,9,12,13,16,17,30,31,32,35,36,37},
-						7:{9,10,13,14,17,18,31,32,33,36,37,38},
-						8:{10,11,14,15,18,19,32,33,34,37,38,39},
-						}
+	tiny_to_big := map[int][]int{
+		0: {0, 1, 4, 5, 8, 9, 20, 21, 22, 25, 26, 27},
+		1: {1, 2, 5, 6, 9, 10, 21, 22, 23, 26, 27, 28},
+		2: {2, 3, 6, 7, 10, 11, 22, 23, 24, 27, 28, 29},
+		3: {4, 5, 8, 9, 12, 13, 25, 26, 27, 30, 31, 32},
+		4: {5, 6, 9, 10, 13, 14, 26, 27, 28, 31, 32, 33},
+		5: {6, 7, 10, 11, 14, 15, 27, 28, 29, 32, 33, 34},
+		6: {8, 9, 12, 13, 16, 17, 30, 31, 32, 35, 36, 37},
+		7: {9, 10, 13, 14, 17, 18, 31, 32, 33, 36, 37, 38},
+		8: {10, 11, 14, 15, 18, 19, 32, 33, 34, 37, 38, 39},
+	}
 
-		// 'a' represents the agentling index
-		for a := 0; a < 9; a++ {
-			// contains all the values
-			tiny_board_values := get_action_list(tiny_envys[a])
+	fmt.Println("Hello6")
+	// 'a' represents the agentling index
+	for a := 0; a < 9; a++ {
+		// contains all the values
+		tiny_board_values := get_action_list(tiny_envys[a])
 
-			for j:= 0; j < 12; j++ {
-				// if edge not avaliable, skip
-				if tiny_board_values[j] == -1 {
-					continue
-				}
-
-				// 1 find corresponding edge in big board
-				big_board_label := tiny_to_big[a][j]
-
-				// update the sum of the values and the count of the number of values contributing to the sum
-				big_board_sums[big_board_label] += tiny_board_values[j]
-				big_board_counts[big_board_label] += 1
-			}			
-		}
-
-		// find the max average value in the big board
-		max_value := -1
-		action := -1
-		for k:=0;k<40;k++{
-			// if there are no counts for the chosen edge, then the chosen edge cant be a valid option
-			if big_board_counts[k] == 0{
+		for j := 0; j < 12; j++ {
+			// if edge not avaliable, skip
+			if tiny_board_values[j] == -1 {
 				continue
 			}
 
-			// calculate average value of taking edge k on the big board	
-			temp_value := (big_board_sums[k] * 1.00) / big_board_counts[k]
+			// 1 find corresponding edge in big board
+			big_board_label := tiny_to_big[a][j]
 
-			// set max edge value, and the the corresponding edge choice
-			if temp_value > max_value{
-				max_value = temp_value
-				action = k
-			}
+			// update the sum of the values and the count of the number of values contributing to the sum
+			big_board_sums[big_board_label] += tiny_board_values[j]
+			big_board_counts[big_board_label] += 1
+		}
+	}
+	fmt.Println("Hello7")
+
+	// find the max average value in the big board
+	max_value := -1
+	action := -1
+	for k := 0; k < 40; k++ {
+		// if there are no counts for the chosen edge, then the chosen edge cant be a valid option
+		if big_board_counts[k] == 0 {
+			continue
 		}
 
-		big_to_vertex := map[int][2]int {
-						0: {0, 5},
-						1: {5, 10},
-						2: {10, 15},
-						3: {15, 20},
-						4: {1, 6},
-						5: {6, 11},
-						6: {11, 16},
-						7: {16, 21},
-						8: {2, 7},
-						9: {7, 12},
-						10: {12, 17},
-						11: {17, 22},
-						12: {3, 8},
-						13: {8, 13},
-						14: {13, 18},
-						15: {18, 23},
-						16: {4, 9},
-						17: {9, 14},
-						18: {14, 19},
-						19: {19, 24},
-						20: {0, 1},
-						21: {5, 6},
-						22: {10, 11},
-						23: {15, 16},
-						24: {20, 21},
-						25: {1, 2},
-						26: {6, 7},
-						27: {11, 12},
-						28: {16, 17},
-						29: {21, 22},
-						30: {2, 3},
-						31: {7, 8},
-						32: {12, 13},
-						33: {17, 18},
-						34: {22, 23},
-						35: {3, 4},
-						36: {8, 9},
-						37: {13, 14},
-						38: {18, 19},
-						39: {23, 24},
-						}
+		// calculate average value of taking edge k on the big board
+		temp_value := (big_board_sums[k] * 1.00) / big_board_counts[k]
 
-		return big_to_vertex[action]
+		// set max edge value, and the the corresponding edge choice
+		if temp_value > max_value {
+			max_value = temp_value
+			action = k
+		}
+	}
+	fmt.Println("Hello8")
+
+	big_to_vertex := map[int][2]int{
+		0:  {0, 5},
+		1:  {5, 10},
+		2:  {10, 15},
+		3:  {15, 20},
+		4:  {1, 6},
+		5:  {6, 11},
+		6:  {11, 16},
+		7:  {16, 21},
+		8:  {2, 7},
+		9:  {7, 12},
+		10: {12, 17},
+		11: {17, 22},
+		12: {3, 8},
+		13: {8, 13},
+		14: {13, 18},
+		15: {18, 23},
+		16: {4, 9},
+		17: {9, 14},
+		18: {14, 19},
+		19: {19, 24},
+		20: {0, 1},
+		21: {5, 6},
+		22: {10, 11},
+		23: {15, 16},
+		24: {20, 21},
+		25: {1, 2},
+		26: {6, 7},
+		27: {11, 12},
+		28: {16, 17},
+		29: {21, 22},
+		30: {2, 3},
+		31: {7, 8},
+		32: {12, 13},
+		33: {17, 18},
+		34: {22, 23},
+		35: {3, 4},
+		36: {8, 9},
+		37: {13, 14},
+		38: {18, 19},
+		39: {23, 24},
+	}
+	fmt.Println("Hello9")
+	return big_to_vertex[action]
 }
-
-
-
-
-
-
-
-
