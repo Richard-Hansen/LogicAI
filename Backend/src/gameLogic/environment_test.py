164bc68646edde246caf639fc746e0a7dc6e7a75
javaComSci <iramanat@purdue.edu>
2019-09-20 15:59:59 -0400

    Hash values

--- Backend/src/gameLogic/environment_test.py
+++ Backend/src/gameLogic/environment_test.py
@@ -8,24 +8,24 @@ class TestEnvironment(unittest.TestCase):
 	# What it is testing: Successfully enumerates all the states
 	# Expected output: I expect that all the states are enumerated without exceeding limits
 	# 
-	# def test_state_enumeration(self):
-	# 	try:
-	# 		m = Envy(areas=[0.05, 0.45, 0.4, 0.1], writeToDB = False)
-	# 		m.create_state_hash_and_values()
-	# 		hash_table = m.get_hash_to_state_and_values()
-	# 	except Exception:
-	# 		self.fail("Creating hash_and_values raised exception when enumerating states")
+	def test_state_enumeration(self):
+		try:
+			m = Envy(areas=[0.05, 0.45, 0.4, 0.1], writeToDB = False)
+			m.create_state_hash_and_values()
+			hash_table = m.get_hash_to_state_and_values()
+		except Exception:
+			self.fail("Creating hash_and_values raised exception when enumerating states")
 
 	# #
 	# # Test Type: Verification Test
 	# # What it is testing: Finds the value of the state that has captured squares
 	# # Expected output: I expect that value for state is calculated correctly
 	# # 
-	# def test_value_calculation(self):
-	# 	m = Envy(areas=[0.05, 0.45, 0.4, 0.1])
-	# 	value = m.calculate_values([1,0,1,0,0,0,2,2,0,0,0,0,1,0,0,0])
+	def test_value_calculation(self):
+		m = Envy(areas=[0.05, 0.45, 0.4, 0.1])
+		value = m.calculate_values([1,0,1,0,0,0,2,2,0,0,0,0,1,0,0,0])
 
-	# 	self.assertEqual(value, 0.525)
+		self.assertEqual(value, 0.525)
 
 	#
 	# Test Type: Verification Test
@@ -37,7 +37,20 @@ class TestEnvironment(unittest.TestCase):
 		start_time = time.time()
 		m.create_state_hash_and_values()
 		total_time = time.time() - start_time
-		# self.assertEqual()
+		self.assertTrue(total_time <= 60)
+
+
+	#
+	# Test Type: Verification Test
+	# What it is testing: Correctness of hash value given state
+	# Expected output: I expect that the hash avlue must be calculated correctly
+	# 
+	def test_hash_values(self):
+		m = Envy(areas=[0.05, 0.45, 0.4, 0.1], writeToDB = False)
+		state_info = [0] * 16
+		state_info[0] = 2
+		hash_code = m.get_hash(state_info)
+		self.assertTrue(hash_code == 28697814)
 		
 if __name__ == '__main__':
     unittest.main()
