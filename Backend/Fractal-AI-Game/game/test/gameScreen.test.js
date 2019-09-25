c256da3db7eb98ea16e4c860efd5a139307def4a
Dellamoresteven <dellamoresteven@gmail.com>
2019-09-25 18:39:51 -0400

    More tests

--- Backend/Fractal-AI-Game/game/test/gameScreen.test.js
+++ Backend/Fractal-AI-Game/game/test/gameScreen.test.js
@@ -7,7 +7,9 @@ const GameScreen = require('../gameScreen/gameScreen.js')
 const GameLogic = require('../gameScreen/gameLogic.js')
 
 var sentRes = "Map1 25 4x4 [[0 0] [0 0.5] [0 2.2] [0 3.7] [0 4] [1.2 0] [1 0.8] [1 1.6] [1 2.7] [2.5 4] [1.6 0] [2 1] [2.5 2.5] [2 3] [2.8 4] [3.4 0] [3 0.4] [3 2.3] [3 3] [3.3 4] [4 0] [4 1] [4 2] [4 3.4] [4 4]] 1,5-2,6-3,7-4,8-9-6,10-7,11-8,12-9,13-14-11,15-12,16-13,17-14,18-19-16,20-17,21-18,22-19,23-24-21-22-23-24"
+var sentSquareRes = "0,1,5,6 1,2,6,7 2,3,7,8 3,4,8,9 5,6,10,11 6,7,11,12 7,8,12,13 8,9,13,14 10,11,15,16 11,12,16,17 12,13,17,18 13,14,18,19 15,16,20,21 16,17,21,22 17,18,22,23 18,19,23,24 0.045625,0.078125,0.081250,0.110938,0.038750,0.081250,0.071875,0.035938,0.061250,0.085937,0.026562,0.046875,0.038750,0.090625,0.065625,0.040625"
 var res;
+var resHttpPostSquareData;
 /* p5js functions and certain DOM functions don't work with mocha
  * resulting in a lot of weird code
  */
@@ -79,8 +81,64 @@ describe('GameScreen tests', function () {
         done()
     })
 
+    it('Check First Square', function (done) {
+        resHttpPostSquareData = gameScreen.mgameLogic.httpPostSquareData(sentSquareRes);
+        expect(resHttpPostSquareData[0][0][0]).to.equal("0")
+        expect(resHttpPostSquareData[0][0][1]).to.equal("1")
+        expect(resHttpPostSquareData[0][0][2]).to.equal("5")
+        expect(resHttpPostSquareData[0][0][3]).to.equal("6")
+        done()
+    })
+
+    it('Check Second Square', function (done) {
+        expect(resHttpPostSquareData[0][1][0]).to.equal("1")
+        expect(resHttpPostSquareData[0][1][1]).to.equal("2")
+        expect(resHttpPostSquareData[0][1][2]).to.equal("6")
+        expect(resHttpPostSquareData[0][1][3]).to.equal("7")
+        done()
+    })
+
+    it('Check Last Square', function (done) {
+        expect(resHttpPostSquareData[0][15][0]).to.equal("18")
+        expect(resHttpPostSquareData[0][15][1]).to.equal("19")
+        expect(resHttpPostSquareData[0][15][2]).to.equal("23")
+        expect(resHttpPostSquareData[0][15][3]).to.equal("24")
+        done()
+    })
+
+    it('Check Area Square One', function (done) {
+        expect(resHttpPostSquareData[1][0]).to.equal("0.045625")
+        done()
+    })
+
+    it('Check other area Square', function (done) {
+        expect(resHttpPostSquareData[1][0]).to.equal("0.045625")
+        expect(resHttpPostSquareData[1][1]).to.equal("0.078125")
+        expect(resHttpPostSquareData[1][2]).to.equal("0.081250")
+        expect(resHttpPostSquareData[1][3]).to.equal("0.110938")
+        expect(resHttpPostSquareData[1][4]).to.equal("0.038750")
+        done()
+    })
+
     it('check player make move', function (done) {
-        gameScreen.mouseClicked();
+        expect(gameScreen.checkPlayerMove(0,1)).to.equal(true)
+        done()
+    })
+
+    it('check duplicate move', function (done) {
+        expect(gameScreen.checkPlayerMove(0,1)).to.equal(false)
+        done()
+    })
+
+    it('check player invalid move', function (done) {
+        expect(gameScreen.checkPlayerMove(0,2)).to.equal(false)
+        done()
+    })
+
+    it('check player takes square', function (done) {
+        expect(gameScreen.checkPlayerMove(0,5)).to.equal(true)
+        expect(gameScreen.checkPlayerMove(5,6)).to.equal(true)
+        expect(gameScreen.checkPlayerMove(1,6)).to.equal(true)
         done()
     })
 })
