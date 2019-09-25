76e096ff35d0688739d2263b4317ddf1b34910c7
javaComSci <iramanat@purdue.edu>
2019-09-25 15:48:51 -0400

    move

--- Backend/src/routes/move.go
+++ Backend/src/routes/move.go
@@ -270,12 +270,12 @@ func GetValue(stateInfos [12][16]int, idd int) [12]float64 {
 		if len(hashCodes) == 0 {
 			hashCodes = `'0_` + fmt.Sprintf("%d",idd) + `_` + hashCode + `'`
 		} else {
-			hashCodes = hashCodes + ",HashCode = '0_" + fmt.Sprintf("%d",idd) + "0_" + hashCode + `'`;
+			hashCodes = hashCodes + ", '0_" + fmt.Sprintf("%d",idd) + "0_" + hashCode + `'`;
 		} 
 		values[i] = -2
 	}
 
-	selectStatement := `SELECT Value FROM hashes WHERE HashCode = (` + hashCodes + `) ORDER BY FIELD(HashCode,` + hashCodes + `(` + `;`
+	selectStatement := `SELECT Value FROM hashes WHERE HashCode IN (` + hashCodes + `) ORDER BY FIELD(HashCode,` + hashCodes + `)` + `;`
 	fmt.Println("----------------------- ", selectStatement)
 
 	// make the call to the database
