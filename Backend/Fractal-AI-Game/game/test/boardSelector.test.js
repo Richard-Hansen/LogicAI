0f4074a0b3965a37df0d39edb624a253475a63a2
colholt <columbus@purdue.edu>
2019-09-25 20:34:08 -0400

    more regression tests, board image file

--- Backend/Fractal-AI-Game/game/test/boardSelector.test.js
+++ Backend/Fractal-AI-Game/game/test/boardSelector.test.js
@@ -12,6 +12,10 @@ describe('BoardSelector tests', function () {
 
     beforeEach(function () {
         global.board = 5
+        global.boardOne = 1
+        global.boardTwo = 2
+        global.boardThree = 3
+        global.activeImage = 0
         boardSelector = new BoardSelector();
     })
 
@@ -20,15 +24,39 @@ describe('BoardSelector tests', function () {
         done()
     })
 
-    it('should change the board', function (done) {
-        boardSelector.changeBoard(4)()
-        expect(board).to.equal(4);
+    it('should change the board to 3', function (done) {
+        boardSelector.changeBoard(3)()
+        expect(board).to.equal(3);
+        done();
+    })
+
+    it('should change the board to 2', function (done) {
+        boardSelector.changeBoard(2)()
+        expect(board).to.equal(2);
+        done();
+    })
+
+    it('should change the board to 1', function (done) {
+        boardSelector.changeBoard(1)()
+        expect(board).to.equal(1);
+        done();
+    })
+
+    it('should not change the board to boards above 3', function (done) {
+        var res = boardSelector.changeBoard(4)()
+        expect(res).to.equal("NO");
+        done();
+    })
+
+    it('should not change the board to boards below 1', function (done) {
+        var res = boardSelector.changeBoard(0)()
+        expect(res).to.equal("NO");
         done();
     })
 
     it('should save the board', function (done) {
         let oldBoard = window.localStorage.getItem("board")
-        boardSelector.changeBoard(20)()
+        boardSelector.changeBoard(2)()
         let newBoard = window.localStorage.getItem("board")
         expect(oldBoard).to.not.equal(newBoard)
         done();
